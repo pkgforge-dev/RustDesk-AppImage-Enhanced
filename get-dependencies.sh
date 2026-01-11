248627fbed5f799a0dbb452e4345d405c289c64a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-11 10:29:04 -0400

    getapp

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,17 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm pipewire-audio pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini
+
+if [ "$ARCH" = 'x86_64' ]; then
+	pacman -S --noconfirm libva-intel-driver
+fi
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package rustdesk
 
 # If the application needs to be manually built that has to be done down here
